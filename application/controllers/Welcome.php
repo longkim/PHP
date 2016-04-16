6cfa023724ebde2e0a9883ec3d13b8963bbebaf6
mryujiawang <821151479@qq.com>
2016-04-16 21:36:02 +1000

    random colour generator
    random colour generator

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -19,4 +19,24 @@ class Welcome extends CI_Controller {
 		$this->load->view('header',$data);
 		$this->load->view('welcome_message');
 	}
+	
+	//Yujia, random colour
+	function randomColor() { 
+    $str = '#'; 
+    for($i = 0 ; $i < 6 ; $i++) { 
+        $randNum = rand(0 , 15); 
+        switch ($randNum) { 
+            case 10: $randNum = 'A'; break; 
+            case 11: $randNum = 'B'; break; 
+            case 12: $randNum = 'C'; break; 
+            case 13: $randNum = 'D'; break; 
+            case 14: $randNum = 'E'; break; 
+            case 15: $randNum = 'F'; break; 
+        } 
+        $str .= $randNum; 
+    } 
+    return $str; 
+} 
+$color = randomColor();
+//Yujia, random colour
 }
