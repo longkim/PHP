648b49ba00737b464ad9ab97942de2a15c958179
mryujiawang <821151479@qq.com>
2016-04-29 22:06:47 +1000

    Product
    Product

--- system/database/DB_forge.php
+++ system/database/DB_forge.php
@@ -44,7 +44,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/user_guide/database/
  */
-abstract class CI_DB_forge {
+abstract class CI_DB_forge 
+{
 
 	/**
 	 * Database object
--- system/database/drivers/mysqli/mysqli_utility.php
+++ system/database/drivers/mysqli/mysqli_utility.php
@@ -46,7 +46,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author		EllisLab Dev Team
  * @link		https://codeigniter.com/user_guide/database/
  */
-class CI_DB_mysqli_utility extends CI_DB_utility {
+class CI_DB_mysqli_utility extends CI_DB_utility 
+{
 
 	/**
 	 * List databases statement
